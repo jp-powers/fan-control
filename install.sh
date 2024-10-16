5bc3264b7aa949de682a490661a2b628a46b23a1
M <martin@iggland.com>
2024-10-16 18:11:55 +0200

    Do not overwrite existing config.ini file with install script

--- install.sh
+++ install.sh
@@ -69,11 +69,13 @@ if [ "$USER_OS" = "1" ]; then
     echo "making appropriate files executable"
     chmod 755 /root/fan-control/gen-config.py /root/fan-control/fan-control.py /root/fan-control/fan-control.sh
     check_script_exec $USER_OS
-    echo "Starting nano to edit the config file generator that now. Ctrl+X when complete to save and exit."
-    echo "(sleeping for 10 seconds to cancel if wanted)"
-    sleep 10
-    nano /root/fan-control/gen-config.py
-    echo "Executing gen-config.py to generate the config file"
+    if [ ! -f config.ini ]; then
+      echo "Starting nano to edit the config file generator that now. Ctrl+X when complete to save and exit."
+      echo "(sleeping for 10 seconds to cancel if wanted)"
+      sleep 10
+      nano /root/fan-control/gen-config.py
+      echo "Executing gen-config.py to generate the config file"
+    fi
     /root/fan-control/gen-config.py
     echo "************************"
     echo "* USER ACTION REQUIRED *"
@@ -104,12 +106,14 @@ if [ "$USER_OS" = "2" ]; then
     echo "making appropriate files executable"
     chmod 755 /root/fan-control/gen-config.py /root/fan-control/fan-control.py
     check_script_exec $USER_OS
-    echo "Starting nano to edit the config file generator that now. Ctrl+X when complete to save and exit."
-    echo "(sleeping for 10 seconds to cancel if wanted)"
-    sleep 10
-    nano /root/fan-control/gen-config.py
-    echo "Executing gen-config.py to generate the config file"
-    /root/fan-control/gen-config.py
+    if [ ! -f config.ini ]; then
+      echo "Starting nano to edit the config file generator that now. Ctrl+X when complete to save and exit."
+      echo "(sleeping for 10 seconds to cancel if wanted)"
+      sleep 10
+      nano /root/fan-control/gen-config.py
+      echo "Executing gen-config.py to generate the config file"
+      /root/fan-control/gen-config.py
+    fi
     echo "Creating link to service file"
     ln -s /root/fan-control/fan-control.service /etc/systemd/system/fan-control.service
     echo "reloading daemons"
